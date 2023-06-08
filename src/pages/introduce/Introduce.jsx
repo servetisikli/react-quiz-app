61b86eed462cfe8a0992756b8d1bff628fb3cc50
servetisikli <servetisikli@gmail.com>
2023-06-08 12:55:14 +0200

    pages css is added

---
+++ src/pages/introduce/Introduce.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ".Introduce.css";
+
+const Introduce = () => {
+  return <div className="instroduce">Introduce</div>;
+};
+
+export default Introduce;
---
+++ src/pages/quiz/Quiz.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ".Quiz.css";
+
+const Quiz = () => {
+  return <div className="quiz">Quiz</div>;
+};
+
+export default Quiz;
